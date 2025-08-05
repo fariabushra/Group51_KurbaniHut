ff2e36e363622e8210e24130d3b27bf8a26c7f9d
fariabushra <bushrafaria4@gmail.com>
2025-08-06 00:30:37 +0600

    seller added

--- src/main/java/com/example/group51_kurbanihut/Faria/SellerController.java
+++ src/main/java/com/example/group51_kurbanihut/Faria/SellerController.java
@@ -1,7 +1,46 @@
 package com.example.group51_kurbanihut.Faria;
 
+import javafx.event.ActionEvent;
+
 public class SellerController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleViewCattleListButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleRemoveCattleButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAddCattleButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleEditDetailsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleUpdateHealthStatusButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleViewSalesButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleBuyerQueriesButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleManageSellerProfileButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogoutButton(ActionEvent actionEvent) {
+    }
+}
