15834312a934d024648036738ce6b85e447a27d2
fariabushra <bushrafaria4@gmail.com>
2025-08-05 17:21:57 +0600

    login added

---
+++ src/main/java/com/example/group51_kurbanihut/LoginController.java
@@ -0,0 +1,24 @@
+package com.example.group51_kurbanihut;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController
+{
+    @javafx.fxml.FXML
+    private PasswordField passwordTextField;
+    @javafx.fxml.FXML
+    private Label errorTextField;
+    @javafx.fxml.FXML
+    private TextField userNameTextfield;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLoginButton(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,12 @@ module com.example.group51_kurbanihut {
 
 
     opens com.example.group51_kurbanihut to javafx.fxml;
+    opens com.example.group51_kurbanihut.Faria to javafx.fxml;
+    opens com.example.group51_kurbanihut.Nahien to javafx.fxml;
+    opens com.example.group51_kurbanihut.Redeta to javafx.fxml;
+
     exports com.example.group51_kurbanihut;
+    exports com.example.group51_kurbanihut.Faria;
+    exports com.example.group51_kurbanihut.Nahien;
+    exports com.example.group51_kurbanihut.Redeta;
 }
