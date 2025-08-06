266fb537f0e470bd3b2886918a4f58575a560335
NahinFarhan07 <nahienfarhanpranto18@gmail.com>
2025-08-07 01:37:42 +0600

    Buyer class

---
+++ src/main/java/com/example/group51_kurbanihut/Nahien/ViewCattleController.java
@@ -0,0 +1,16 @@
+package com.example.group51_kurbanihut.Nahien;
+
+public class ViewCattleController
+{
+    @javafx.fxml.FXML
+    private TableColumn cattleNameTableCol;
+    @javafx.fxml.FXML
+    private TableView cattleTableCol;
+    @javafx.fxml.FXML
+    private TableColumn cattleIdTableCol;
+    @javafx.fxml.FXML
+    private TableColumn cattlePriceTableCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
