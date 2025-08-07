799c4401be31bcd12520c74d8ff4aba770f519e0
NahinFarhan07 <nahienfarhanpranto18@gmail.com>
2025-08-07 22:29:15 +0600

    Buyer class

--- src/main/java/com/example/group51_kurbanihut/Nahien/CustomerServiceOfficerDashboardController.java
+++ src/main/java/com/example/group51_kurbanihut/Nahien/CustomerServiceOfficerDashboardController.java
@@ -1,7 +1,42 @@
 package com.example.group51_kurbanihut.Nahien;
 
+import javafx.event.ActionEvent;
+
 public class CustomerServiceOfficerDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void topPriorityButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void solveFeedbackButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void feedbackReplyButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void deleteSolveFeedbackButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void paymentReminderButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateFaqButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void feedbackButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void flagAsSuspiciousButton(ActionEvent actionEvent) {
+    }
+}
