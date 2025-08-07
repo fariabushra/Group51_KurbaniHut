53d89f4a9fc3019444b6c7fda81222e4cc7d507f
NahinFarhan07 <nahienfarhanpranto18@gmail.com>
2025-08-07 19:52:48 +0600

    Buyer class

---
+++ src/main/java/com/example/group51_kurbanihut/Nahien/FeedbackReplyController.java
@@ -0,0 +1,27 @@
+package com.example.group51_kurbanihut.Nahien;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+public class FeedbackReplyController
+{
+    @javafx.fxml.FXML
+    private ListView feedbackListView;
+    @javafx.fxml.FXML
+    private TextArea replyTextArea;
+    @javafx.fxml.FXML
+    private TextArea feedbackDetailTextArea;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void previousButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sendFeedbackButton(ActionEvent actionEvent) {
+    }
+}
