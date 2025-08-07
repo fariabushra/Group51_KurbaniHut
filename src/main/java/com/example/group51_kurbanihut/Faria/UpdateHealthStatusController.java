648dcd0f0ba5a291b20cdda74e0b09c4f21f1aea
fariabushra <bushrafaria4@gmail.com>
2025-08-07 18:48:12 +0600

    update health status added

--- src/main/java/com/example/group51_kurbanihut/Faria/UpdateHealthStatusController.java
+++ src/main/java/com/example/group51_kurbanihut/Faria/UpdateHealthStatusController.java
@@ -1,23 +1,30 @@
 package com.example.group51_kurbanihut.Faria;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
-public class ViewSalesController
+public class UpdateHealthStatusController
 {
     @javafx.fxml.FXML
-    private TableColumn historyTableCol;
+    private Label updateTextLabel;
     @javafx.fxml.FXML
-    private TableView cattleHistoryTableView;
+    private ComboBox updateHealthComboBox;
     @javafx.fxml.FXML
-    private TableColumn cattleHistoryIDTableCol;
+    private TableColumn cattleIDTableCol;
     @javafx.fxml.FXML
-    private Label totalSaleLabel;
+    private TableColumn healthStatusTableCol;
+    @javafx.fxml.FXML
+    private TableView healthStatusTableView;
 
     @javafx.fxml.FXML
     public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleUpdateStatusButton(ActionEvent actionEvent) {
     }
 
     @javafx.fxml.FXML
