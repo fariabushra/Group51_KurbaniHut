956b5e22f611f393e71e8781f726466ea118c18a
NahinFarhan07 <nahienfarhanpranto18@gmail.com>
2025-08-07 20:33:44 +0600

    Buyer class

---
+++ src/main/java/com/example/group51_kurbanihut/Nahien/TopPriorityFeedbackController.java
@@ -0,0 +1,30 @@
+package com.example.group51_kurbanihut.Nahien;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class TopPriorityFeedbackController
+{
+    @javafx.fxml.FXML
+    private TableColumn feedbackColumn;
+    @javafx.fxml.FXML
+    private Label titleLabel;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private TableView feedbackTable;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void previousButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void markTopPriorityButton(ActionEvent actionEvent) {
+    }
+}
