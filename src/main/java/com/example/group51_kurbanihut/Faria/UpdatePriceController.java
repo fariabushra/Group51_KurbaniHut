fe698ba8cf65c016f6b5928caa87eebf1cb9a25e
fariabushra <bushrafaria4@gmail.com>
2025-08-07 17:01:15 +0600

    update price added

---
+++ src/main/java/com/example/group51_kurbanihut/Faria/UpdatePriceController.java
@@ -0,0 +1,33 @@
+package com.example.group51_kurbanihut.Faria;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class UpdatePriceController
+{
+    @javafx.fxml.FXML
+    private Label updateTextLabel;
+    @javafx.fxml.FXML
+    private TableColumn priceTableCol;
+    @javafx.fxml.FXML
+    private TableColumn idTableCol;
+    @javafx.fxml.FXML
+    private TextField priceUpdateTextField;
+    @javafx.fxml.FXML
+    private TableView priceTableView;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleUpdateButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDashboardButton(ActionEvent actionEvent) {
+    }
+}
