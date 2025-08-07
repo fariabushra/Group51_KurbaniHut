cde1736c631c0fff18ffcf4a3f42d6730aec813d
fariabushra <bushrafaria4@gmail.com>
2025-08-07 21:17:35 +0600

    manage profile added

---
+++ src/main/java/com/example/group51_kurbanihut/Faria/ManageSellerProfileController.java
@@ -0,0 +1,30 @@
+package com.example.group51_kurbanihut.Faria;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class ManageSellerProfileController
+{
+    @javafx.fxml.FXML
+    private TextField userNameTextField;
+    @javafx.fxml.FXML
+    private TextField phoneNoTextField;
+    @javafx.fxml.FXML
+    private TextField permanentAddressTextField;
+    @javafx.fxml.FXML
+    private TextField emailTextField;
+    @javafx.fxml.FXML
+    private TextField nameTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDashboardButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSaveChangesButton(ActionEvent actionEvent) {
+    }
+}
