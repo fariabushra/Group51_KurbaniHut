1110e513f4fb3fec65adb498bf13680611c5a042
fariabushra <bushrafaria4@gmail.com>
2025-08-07 18:06:55 +0600

    view sales added

---
+++ src/main/java/com/example/group51_kurbanihut/Faria/ViewSalesController.java
@@ -0,0 +1,23 @@
+package com.example.group51_kurbanihut.Faria;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ViewSalesController
+{
+    @javafx.fxml.FXML
+    private TableColumn historyTableCol;
+    @javafx.fxml.FXML
+    private TableView cattleHistoryTableView;
+    @javafx.fxml.FXML
+    private TableColumn cattleHistoryIDTableCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDashboardButton(ActionEvent actionEvent) {
+    }
+}
