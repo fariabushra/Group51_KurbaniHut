0c7eb843a4c390671c325c0d1fc490af8d017bb8
NahinFarhan07 <nahienfarhanpranto18@gmail.com>
2025-08-07 22:19:45 +0600

    Buyer class

--- src/main/java/com/example/group51_kurbanihut/Nahien/BuyerDashboardController.java
+++ src/main/java/com/example/group51_kurbanihut/Nahien/BuyerDashboardController.java
@@ -1,7 +1,42 @@
 package com.example.group51_kurbanihut.Nahien;
 
+import javafx.event.ActionEvent;
+
 public class BuyerDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void cattlelIstButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addToFavouriteButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void paymentButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void bookCatlleButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchCattleByTypeButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cancelBookingButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sendFeedbackButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewCattleDetailsButton(ActionEvent actionEvent) {
+    }
+}
