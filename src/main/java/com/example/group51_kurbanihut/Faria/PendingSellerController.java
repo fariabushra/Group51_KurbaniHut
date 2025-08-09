fc56a7dfd75d6b13b8ce4e3d24160bfa77af4c13
fariabushra <bushrafaria4@gmail.com>
2025-08-09 16:53:31 +0600

    pending seller added

--- src/main/java/com/example/group51_kurbanihut/Faria/PendingSellerController.java
+++ src/main/java/com/example/group51_kurbanihut/Faria/PendingSellerController.java
@@ -1,7 +1,34 @@
 package com.example.group51_kurbanihut.Faria;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class PendingSellerController
 {
     @javafx.fxml.FXML
+    private TableColumn sellerUserNameTableCol;
+    @javafx.fxml.FXML
+    private TableView pendingSellerTableView;
+    @javafx.fxml.FXML
+    private Label textLabel;
+    @javafx.fxml.FXML
+    private TableColumn sellerFullNameTableCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleRejectButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDashboardButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleApproveButton(ActionEvent actionEvent) {
+    }
+}
