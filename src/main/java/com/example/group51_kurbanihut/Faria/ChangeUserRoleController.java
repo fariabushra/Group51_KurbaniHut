3362f3f27e7dc067013cd86c90159a30406e4c8e
fariabushra <bushrafaria4@gmail.com>
2025-08-09 16:28:59 +0600

    change user role added

--- src/main/java/com/example/group51_kurbanihut/Faria/ChangeUserRoleController.java
+++ src/main/java/com/example/group51_kurbanihut/Faria/ChangeUserRoleController.java
@@ -1,7 +1,33 @@
 package com.example.group51_kurbanihut.Faria;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ChangeUserRoleController
 {
     @javafx.fxml.FXML
+    private TableColumn userNameTableCol;
+    @javafx.fxml.FXML
+    private ComboBox roleComboBox;
+    @javafx.fxml.FXML
+    private TableColumn fullNameTableCol;
+    @javafx.fxml.FXML
+    private TableView changeUserTableView;
+    @javafx.fxml.FXML
+    private Label roleChangeTextlabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleSaveButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDashboardButton(ActionEvent actionEvent) {
+    }
+}
