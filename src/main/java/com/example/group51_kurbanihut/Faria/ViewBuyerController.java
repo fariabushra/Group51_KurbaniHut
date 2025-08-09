25cfd264634fab87c6fa26505f584a8e6ff063af
fariabushra <bushrafaria4@gmail.com>
2025-08-09 15:05:58 +0600

    view buyer added

--- src/main/java/com/example/group51_kurbanihut/Faria/ViewBuyerController.java
+++ src/main/java/com/example/group51_kurbanihut/Faria/ViewBuyerController.java
@@ -1,7 +1,27 @@
 package com.example.group51_kurbanihut.Faria;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ViewBuyerController
 {
     @javafx.fxml.FXML
+    private TableColumn buyerFullNameTableCol;
+    @javafx.fxml.FXML
+    private TableColumn buyerUserNameTableCol;
+    @javafx.fxml.FXML
+    private TableView buyerTableView;
+    @javafx.fxml.FXML
+    private TableColumn buyerPhoneNoTableCol;
+    @javafx.fxml.FXML
+    private TableColumn buyerEmailTableCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleDashboardButton(ActionEvent actionEvent) {
+    }
+}
