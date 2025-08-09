ecd6f928806397c060a34dbf4eb05411f07b2e34
fariabushra <bushrafaria4@gmail.com>
2025-08-09 22:05:34 +0600

    sale cattle added

---
+++ src/main/java/com/example/group51_kurbanihut/Faria/SaleCattleController.java
@@ -0,0 +1,40 @@
+package com.example.group51_kurbanihut.Faria;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class SaleCattleController
+{
+    @javafx.fxml.FXML
+    private TableColumn cattleIDTableCol;
+    @javafx.fxml.FXML
+    private TableColumn weightTableCol;
+    @javafx.fxml.FXML
+    private TableColumn healthStatusTableCol;
+    @javafx.fxml.FXML
+    private TableColumn heightTableCol;
+    @javafx.fxml.FXML
+    private TableColumn priceTableCol;
+    @javafx.fxml.FXML
+    private Label saleTextLabel;
+    @javafx.fxml.FXML
+    private TableView saleCattleTableView;
+    @javafx.fxml.FXML
+    private TableColumn colourTableCol;
+    @javafx.fxml.FXML
+    private TableColumn ageTableCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSaleCattleButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDashboardButton(ActionEvent actionEvent) {
+    }
+}
