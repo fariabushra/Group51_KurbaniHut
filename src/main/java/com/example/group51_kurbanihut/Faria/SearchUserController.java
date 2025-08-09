783bef3305d785a84f34363e58213bae8f4c38b5
fariabushra <bushrafaria4@gmail.com>
2025-08-09 15:34:48 +0600

    search user added

--- src/main/java/com/example/group51_kurbanihut/Faria/SearchUserController.java
+++ src/main/java/com/example/group51_kurbanihut/Faria/SearchUserController.java
@@ -1,7 +1,32 @@
 package com.example.group51_kurbanihut.Faria;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class SearchUserController
 {
     @javafx.fxml.FXML
+    private TableColumn userEmailTableCol;
+    @javafx.fxml.FXML
+    private TableColumn userPhoneNoTableCol;
+    @javafx.fxml.FXML
+    private TableView searchUserTableView;
+    @javafx.fxml.FXML
+    private TextField searchUserTextField;
+    @javafx.fxml.FXML
+    private TableColumn searchUserNameTableCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleSearchButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDashboardButton(ActionEvent actionEvent) {
+    }
+}
