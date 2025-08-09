6a5bd309ac1d691eaba7ef6a5fe61d8cb2201012
fariabushra <bushrafaria4@gmail.com>
2025-08-09 15:55:01 +0600

    deactivate user added

--- src/main/java/com/example/group51_kurbanihut/Faria/DeactivateUserController.java
+++ src/main/java/com/example/group51_kurbanihut/Faria/DeactivateUserController.java
@@ -1,7 +1,32 @@
 package com.example.group51_kurbanihut.Faria;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class DeactivateUserController
 {
     @javafx.fxml.FXML
+    private TableColumn userNameTableCol;
+    @javafx.fxml.FXML
+    private Label deactiveTextLabel;
+    @javafx.fxml.FXML
+    private TableColumn fullNameTableCol;
+    @javafx.fxml.FXML
+    private TableView deactivateUserTableView;
+    @javafx.fxml.FXML
+    private TableColumn phoneNoTableCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleDeactivateAccountButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDashboardButton(ActionEvent actionEvent) {
+    }
+}
