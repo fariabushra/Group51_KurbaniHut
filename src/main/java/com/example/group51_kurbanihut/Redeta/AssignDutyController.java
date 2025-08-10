8e5beb336f46a106653bf1e6289468a746a95b79
redetazaman <zamanredeta@gmail.com>
2025-08-10 15:44:32 +0600

    security fxml

---
+++ src/main/java/com/example/group51_kurbanihut/Redeta/AssignDutyController.java
@@ -0,0 +1,34 @@
+package com.example.group51_kurbanihut.Redeta;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class AssignDutyController
+{
+    @javafx.fxml.FXML
+    private TableColumn assignedZoneTableCol;
+    @javafx.fxml.FXML
+    private ComboBox zoneComboBox;
+    @javafx.fxml.FXML
+    private TextField nameTextField;
+    @javafx.fxml.FXML
+    private Label confirmationMessageLabel;
+    @javafx.fxml.FXML
+    private TableColumn officerNameTableCol;
+    @javafx.fxml.FXML
+    private TableView assignDutyTableView;
+    @javafx.fxml.FXML
+    private Label assignDutyLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void assignZoneOnHandle(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnHandle(ActionEvent actionEvent) {
+    }
+}
