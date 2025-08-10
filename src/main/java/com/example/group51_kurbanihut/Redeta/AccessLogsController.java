c41096a60425289c50d33302c9c82907556c2c9d
redetazaman <zamanredeta@gmail.com>
2025-08-10 16:12:11 +0600

    security fxml

---
+++ src/main/java/com/example/group51_kurbanihut/Redeta/AccessLogsController.java
@@ -0,0 +1,30 @@
+package com.example.group51_kurbanihut.Redeta;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class AccessLogsController
+{
+    @javafx.fxml.FXML
+    private TableColumn nameTableCol;
+    @javafx.fxml.FXML
+    private Label accessLogsLabel;
+    @javafx.fxml.FXML
+    private TableView accessLogsTableView;
+    @javafx.fxml.FXML
+    private TableColumn entryTimeTableCol;
+    @javafx.fxml.FXML
+    private DatePicker selectDateDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn locationTableCol;
+    @javafx.fxml.FXML
+    private ComboBox selectLocationComboBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void searchButtonOnAction(ActionEvent actionEvent) {
+    }
+}
