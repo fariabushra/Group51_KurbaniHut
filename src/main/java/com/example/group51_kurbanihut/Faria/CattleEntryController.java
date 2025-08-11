ac1e5bc87d61202bc708bc36864d99ab105ca2e0
fariabushra <bushrafaria4@gmail.com>
2025-08-11 21:37:04 +0600

    updated

--- src/main/java/com/example/group51_kurbanihut/Faria/CattleEntryController.java
+++ src/main/java/com/example/group51_kurbanihut/Faria/CattleEntryController.java
@@ -1,8 +1,13 @@
 package com.example.group51_kurbanihut.Faria;
 
+import com.example.group51_kurbanihut.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class CattleEntryController
 {
@@ -27,5 +32,19 @@ public class CattleEntryController
 
     @javafx.fxml.FXML
     public void handleSubmitButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDashboardButton(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Faria/Seller.fxml"));
+            Scene nextScene = new Scene(fxmlLoader.load());
+            Stage nextStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            nextStage.setTitle("Seller Dashboard");
+            nextStage.setScene(nextScene);
+            nextStage.show();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
